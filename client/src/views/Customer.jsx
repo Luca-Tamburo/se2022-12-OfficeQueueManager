68231b18abb056c864b89340f6e3adfa0d0f912e
Luca Tamburo <luca.jonk7@hotmail.it>
2022-10-18 16:58:13 +0200

    Fix Customer Page
    - Inserted a map to iterate on the button component
    - Created an array of objects to dynamically change icons

--- client/src/views/Customer.jsx
+++ client/src/views/Customer.jsx
@@ -1,53 +1,86 @@
 //Imports
+import { useState, useEffect } from 'react';
 import { Button, Col, Row, Container } from "react-bootstrap";
-import Display from '../components/Display'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBuildingColumns, faWallet, faTruckFast, faList } from '@fortawesome/free-solid-svg-icons';
+
+//Components
+import Display from '../components/Display'
+
+//Hooks
+import useNotification from '../hooks/useNotification';
+
+//APIs
 import api from "../API";
 
-const Customer = async (props) => {
-    try{
-        let service = await api.getService();
-   
+const Customer = () => {
+    const [services, setServices] = useState([])
+    const notify = useNotification();
+
+    const icons = [
+        {
+            service_id: 1,
+            name: faBuildingColumns
+        },
+        {
+            service_id: 2,
+            name: faWallet
+        },
+        {
+            service_id: 3,
+            name: faTruckFast
+        },
+        {
+            service_id: 4,
+            name: faList
+        }
+    ]
+
+    useEffect(() => {
+        api.getService()
+            .then((services) => {
+                setServices(services.rows);
+            })
+            .catch(err => {
+                notify.error(err.message);
+            })
+    }, []) //eslint-disable-line react-hooks/exhaustive-deps
+
+
     return (
         <Container className='h-100 d-flex flex-column align-item-center justify-content-center'>
             <Row>
                 <h2 className="text-center mb-5">Choose your service</h2>
-                <div>
+                <Col className="mb-4 mt-2 text-center">
                     {
-                        service.rows.map((type,index) => {
-                            return(
-                                <div key = {index}>
-                                    <Button size="lg" className="mx-4 p-2" onClick={() => { api.addTicket(1) }}>
-                                        <FontAwesomeIcon icon={faBuildingColumns} className='pe-2' />
-                                        {type.Name}
-                                    </Button>
-                                </div>   
+                        services.map((service, index) => {
+                            const icon = icons.filter(i => i.service_id === service.ID)[0].name
+                            console.log(icon);
+                            return (
+                                <Button key={index} size="lg" className="mx-4 p-2" onClick={() => { api.addTicket(service.ID) }}>
+                                    <FontAwesomeIcon icon={icon} className='pe-2' />
+                                    {service.Name}
+                                </Button>
                             )
                         })
                     }
-                </div>
+                </Col>
             </Row>
-
+            {/* Add props here */}
             <Row className="mt-4 text-center">
                 <Display />
             </Row>
         </Container>
     );
 }
-catch(err){
-    throw err;}
-}
+
 
 /*
 //Hooks
 import useNotification from '../hooks/useNotification';
-
 import { useNavigate } from "react-router-dom";
-
     const notify = useNotification();
     const navigate = useNavigate();
-
     function takeTicket(service_id) {
         api.addTicket(service_id);
         notify.success(`Operation performed successfully!. You have ticket # ${ticket.id} for the service ${service.id}. Waiting time ${ticket.id}`)
@@ -56,24 +89,3 @@ import { useNavigate } from "react-router-dom";
 */
 
 export default Customer;
-
-// {/* Add props here */}
-// <Button size="lg" className="mx-4 p-2" onClick={() => { api.addTicket(1) }}>
-// <FontAwesomeIcon icon={faBuildingColumns} className='pe-2' />
-// Service 1
-// </Button>
-// {/* Add props here */}
-// <Button size="lg" className="mx-4 p-2" onClick={() => { api.addTicket(2) }}>
-// <FontAwesomeIcon icon={faWallet} className='pe-2' />
-// Service 2
-// </Button>
-// {/* Add props here */}
-// <Button size="lg" className="mx-4 p-2" onClick={() => { api.addTicket(3) }}>
-// <FontAwesomeIcon icon={faTruckFast} className='pe-2' />
-// Service 3
-// </Button>
-// {/* Add props here */}
-// <Button size="lg" className="mx-4 p-2" onClick={() => { api.addTicket(4) }}>
-// <FontAwesomeIcon icon={faList} className='pe-2' />
-// Service 4
-// </Button>
